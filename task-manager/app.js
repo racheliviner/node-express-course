ad84a153340f95985b621f3f0ab4e9311c3daacf
Racheli <viderracheli@gamil.com>
2024-10-29 16:03:28 +0200

    connect mongo db

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks.js')
+const connectDB = require('./mongoDB/connect.js')
+require('dotenv').config()
 
-//routes
 app.use(express.json());
 
+//routes
 app.use('/api/v1/tasks', tasks)
 
-
-
-
-
+//start the server
 const port = 3000
-app.listen(port, console.log(`Server is listening on port ${port}...`))
+
+async function start(){
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))     
+    } catch (error) {
+        console.log("Error connecting to the DB:", error);
+    }
+}
+
+start()
+
